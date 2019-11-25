4d42e0c97a5ff75a2e1cf2c15050d3b368d25370
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-11-26 00:43:45 +0300

    Remove orphain code

--- src/main/java/io/github/rmuhamedgaliev/woodpecker/HelloController.java
+++ src/main/java/io/github/rmuhamedgaliev/woodpecker/HelloController.java
@@ -1,6 +1,7 @@
 package io.github.rmuhamedgaliev.woodpecker;
 
 import io.github.rmuhamedgaliev.woodpecker.commands.YoutrackCommands;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.logging.Level;
 
+@Log
 @Component
 public class HelloController extends TelegramLongPollingBot {
 
@@ -39,12 +42,15 @@ public class HelloController extends TelegramLongPollingBot {
             String methodName = message.split(" ")[0].replaceAll("/", "");
 
             Method method = youtrackCommands.getClass().getMethod(methodName, Update.class);
+
             method.invoke(youtrackCommands, update);
 
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+        } catch (NoSuchMethodException e) {
+            log.log(Level.WARNING, "Method not found " + e.getMessage());
+        } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
-//        }
+
     }
 
     @Override
